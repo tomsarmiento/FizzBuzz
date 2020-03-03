14b7bede2915cc4d9af938f2369f0b21cb419e63
smoothlyraining <tomas.sarmiento@gmail.com>
2020-03-03 11:02:46 -0300

    Listo

---
+++ FizzBuzz.java
@@ -0,0 +1,17 @@
+public class FizzBuzz {
+    public String fizzBuzz(Integer number) {
+        if(number%3==0 && number%5!=0){
+            return "Fizz";
+        }
+        else if(number%5==0 && number%3!=0){
+            return "Buzz";
+        }
+        else if(number%5==0 && number%3==0){
+            return "FizzBuzz";
+        }
+        else{
+            String z = number+""; 
+            return (z); // it can also be done as "return String.valueOf(number)"
+        }
+    }
+}
---
+++ FizzBuzzTest.java
@@ -0,0 +1,13 @@
+public class FizzBuzzTest{
+    public static void main(String[] args){
+        FizzBuzz iD = new FizzBuzz();
+        String x = iD.fizzBuzz(3);
+        System.out.println(x);
+        String y = iD.fizzBuzz(5);
+        System.out.println(y);
+        String z = iD.fizzBuzz(15);
+        System.out.println(z);
+        String a = iD.fizzBuzz(2);
+        System.out.println(a);
+    }
+}
